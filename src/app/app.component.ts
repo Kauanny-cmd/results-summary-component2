3f74fd5b8a7b66e36b22ebc4b3ec3bc274ef7765
kauanny-cmd <kauannyvieira8@gmail.com>
2024-05-17 07:36:52 -0300

    feat: rename

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import data from '../data/data.json';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
 export class AppComponent {
-  title = 'results-summary-component2';
+  items = data;
 }
